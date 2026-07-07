1707741f427f91ae91a960f323156f4cfd528cc2
andres <andreseduardo32163@gmail.com>
2026-07-07 13:25:40 -0400

    Update email_service.py
    se actualizo email

--- email_service.py
+++ email_service.py
@@ -1,10 +1,10 @@
 # email_service.py (correo desactivado para pruebas) se utiliza este ya que es para simulaciones, "email_service_orig.py" es el que realmente hace la funcion, no se implementa porque no funciona en hosting gratuito", al pgar un host se implementará
 
-def enviar_correo_restablecimiento(*args, **kwargs):
+def enviar_correo_restablecimiento(email, token):
     print(f"[SIMULADO] Se enviaría enlace de restablecimiento a {email} con token {token}")
     return True, None
 
-def enviar_correo_confirmacion(*args, **kwargs):
+def enviar_correo_confirmacion(email, token):
     print(f"[SIMULADO] Se enviaría enlace de confirmación a {email} con token {token}")
     return True, None
 
